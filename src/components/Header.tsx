0e6fa82f04534453b5adbedca74df04ba9619caf
Sebastian Sonesen <35290401+sebsone@users.noreply.github.com>
2025-05-26 20:44:31 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
       <h1 className="text-5xl md:text-6xl font-bold text-yellow-400 mb-2">
         Egg Timer
       </h1>
-      <p className="text-lg text-yellow-400">Perfect eggs, every time!</p>
+      <h2 className="text-lg text-yellow-400">Perfect eggs, every time!</h2>
     </header>
   )
 }
